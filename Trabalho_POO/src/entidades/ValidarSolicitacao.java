0706ed22898046285c2e65ed45ae3ea75a79adc3
Davi de Sá Luquini <126691508+DaviLuquini@users.noreply.github.com>
2023-12-02 16:57:46 -0300

    Adicionado SolicitarLaboratorio e ValidarSolicitacao

---
+++ Trabalho_POO/src/entidades/ValidarSolicitacao.java
@@ -0,0 +1,21 @@
+class ValidarSolicitacao {
+
+    public ResultadoSolicitacao validar(Solicitacao solicitacao) {
+
+        if (solicitacao.getProfessor().getReservasAprovadas() >= 6) {
+            return ResultadoSolicitacao.LIMITE_ATINGIDO;
+        }
+
+
+        for (Solicitacao reserva : solicitacao.getLaboratorio().getReservas()) {
+            if (reserva.getSituacao() == ResultadoSolicitacao.APROVADO) {
+                if (solicitacao.getData().equals(reserva.getData()) &&
+                        solicitacao.getTempo() == reserva.getTempo()) {
+                    return ResultadoSolicitacao.CONFLITO;
+                }
+            }
+        }
+
+        return ResultadoSolicitacao.APROVADO;
+    }
+}
