d5927be159f54e2f552826bd483c526519642c53
Maria Fernanda Prandi <127303256+Mafeepam@users.noreply.github.com>
2023-12-02 15:31:27 -0300

    Add files via upload

---
+++ Trabalho_POO/src/entidades/Solicitacao.java
@@ -0,0 +1,58 @@
+package entidades;
+
+import java.util.List;
+
+public class Solicitacao {
+	
+	private Laboratorio laboratorio;
+	private Professor professor;
+	private Disciplina disciplina;
+	private List<Aluno>getAlunos;
+	private LocalDate data;
+	private int tempo;
+	
+	public Laboratorio getLaboratorio() {
+		return laboratorio;
+	}
+	public void setLaboratorio(Laboratorio laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+	public Professor getProfessor() {
+		return professor;
+	}
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	public List<Aluno> getGetAlunos() {
+		return getAlunos;
+	}
+	public void setGetAlunos(List<Aluno> getAlunos) {
+		this.getAlunos = getAlunos;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	public int getTempo() {
+		return tempo;
+	}
+	public void setTempo(int tempo) {
+		this.tempo = tempo;
+	}
+	
+	@Override
+	public String toString() {
+	
+		return super.toString();
+	}
+	
+
+}
