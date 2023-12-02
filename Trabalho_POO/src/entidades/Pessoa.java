28430cf5c206587a60d08077332edebdbf22f1bb
Maria Fernanda Prandi <127303256+Mafeepam@users.noreply.github.com>
2023-12-02 18:22:02 -0300

    Update Pessoa.java

--- Trabalho_POO/src/entidades/Pessoa.java
+++ Trabalho_POO/src/entidades/Pessoa.java
@@ -1,5 +1,21 @@
 package entidades;
 
 public class Pessoa {
-
+  
+private String nome;
+	private boolean status;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
 }
