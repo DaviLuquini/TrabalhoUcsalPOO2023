3dbc2cde6b2172197683ed5af3da13f94454a582
Davi de Sá Luquini <126691508+DaviLuquini@users.noreply.github.com>
2023-12-03 19:50:19 -0300

    Update Solicitacao.java

--- Trabalho_POO/src/entidades/Solicitacao.java
+++ Trabalho_POO/src/entidades/Solicitacao.java
@@ -4,57 +4,73 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Solicitacao implements Reservavel{
+	rivate List<Laboratorio> laboratorio;
+	private List<Professor> professor;
+	private List<Disciplina> disciplina;
+	private List<Aluno> Alunos;
+	private List<LocalDate> data;
+	private List<Integer> tempo;
 	
-	private Laboratorio laboratorio;
-	private Professor professor;
-	private Disciplina disciplina;
-	private List<Aluno> alunos;
-	private LocalDate data;
-	private int tempo;
-	
-	public Laboratorio getLaboratorio() {
-		return laboratorio;
+	public List<Laboratorio> getLaboratorio() {
+	    return laboratorio;
 	}
-	public void setLaboratorio(Laboratorio laboratorio) {
-		this.laboratorio = laboratorio;
+
+	public void setLaboratorio(List<Laboratorio> laboratorio) {
+	    this.laboratorio = laboratorio;
 	}
-	public Professor getProfessor() {
-		return professor;
+
+	public List<Professor> getProfessor() {
+	    return professor;
 	}
-	public void setProfessor(Professor professor) {
-		this.professor = professor;
+
+	public void setProfessor(List<Professor> professor) {
+	    this.professor = professor;
 	}
-	public Disciplina getDisciplina() {
-		return disciplina;
+
+	public List<Disciplina> getDisciplina() {
+	    return disciplina;
 	}
-	public void setDisciplina(Disciplina disciplina) {
-		this.disciplina = disciplina;
+
+	public void setDisciplina(List<Disciplina> disciplina) {
+	    this.disciplina = disciplina;
 	}
 
 	public List<Aluno> getAlunos() {
-		return alunos;
+	    return Alunos;
 	}
-	public void setAlunos(List<Aluno> alunos) {
-		this.alunos = alunos;
+
+	public void setAlunos(List<Aluno> Alunos) {
+	    this.Alunos = Alunos;
 	}
+
 	public LocalDate getData() {
-		return data;
+	    return data;
 	}
+
 	public void setData(LocalDate data) {
-		this.data = data;
+	    this.data = data;
 	}
-	public int getTempo() {
-		return tempo;
+
+	public List<Integer> getTempo() {
+	    return tempo;
 	}
-	public void setTempo(int tempo) {
-		this.tempo = tempo;
+
+	public void setTempo(List<Integer> tempo) {
+	    this.tempo = tempo;
 	}
-	
+
 	@Override
 	public String toString() {
-	
-		return super.toString();
+	    return "Aula{" +
+	            "laboratorio=" + laboratorio +
+	            ", professor=" + professor +
+	            ", disciplina=" + disciplina +
+	            ", alunos=" + Alunos +
+	            ", data=" + data +
+	            ", tempo=" + tempo +
+	            '}';
 	}
+
 	@Override
 	public boolean estaDisponivel(LocalDate data, int tempoEmMinutos) {
 		 // Retorna true se estiver disponível, false caso contrário
